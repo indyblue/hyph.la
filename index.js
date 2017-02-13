a60e2290d71e79947509313568dd07bf2048a7a6
DAS <david@indyblue.com>
2017-02-12 19:50:12 -0500

    hyph - combine some for loops - remove 3rd cons loop for begin/end word groups...was blowing out file size - instead get list of 3+ cons groups from brevrom and whitaker - write latin hyph dict to file so that we can use it w/ jquery - "fix" jquery hypher version to work with my word mods

--- index.js
+++ index.js
@@ -24,48 +24,59 @@ var a1='\u00e1', e1='\u00e9', i1='\u00ed', o1='\u00f3', u1='\u00fa', y1='\u00fd'
 var vowel = 'aeiouy'.split('').concat([a1,e1,i1,o1,u1,y1,e2,ae,oe,ae1,oe1]),
 	cons = 'bcdfghjklmnpqrstvwxz'.split('');
 
-var all = [];
+// vowel consonant pairs
 for(var i of vowel){
 for(var j of cons){
 	addpat(i+'1'+j);
 }}
-for(var i of vowel){
-for(var j of vowel){
-	addpat(i+'1'+j);
-}}
+
+// special cases, or vowel vowel pairs
 for(var i of vowel){
 	addpat('ti8'+i);
 	addpat('t'+i1+'8'+i);
 	addpat('q8u8'+i);
 	addpat('ngu8'+i);
+	for(var j of vowel) addpat(i+'1'+j);
 }
+
+// au dipthong
 addpat('a2u', 'a2'+u1, a1+'2u', a1+'2'+u1);
 
+// multi consonant pairs
 for(var i of cons){
-for(var j of cons){
-	addpat('2'+i+'1'+j);
-}}
+	addpat('8'+i+'_');
+	for(var j of cons){
+		addpat('2'+i+'1'+j);
+		addpat('_'+i+'8'+j+'8');
+		addpat('8'+i+'8'+j+'_');
+		/* // this third full loop makes file much too large - goes from 10kb to 120kb
+		for(var k of cons){
+			//addpat('_'+i+'8'+j+'8'+k+'8');
+			//addpat('8'+i+'8'+j+'8'+k+'_');
+		} */
+	}
+}
 
-for(var i of cons){
-for(var j of cons){
-	addpat('_'+i+'8'+j+'8');
-	addpat('8'+i+'8'+j+'_');
-}}
+// 3+ cons groups, beginning/end of word. 
+// get all groups from brevrom website and from whitaker's listall
+// probably a lot of these will never occur at the beginning/end of word, but what the heck.
+// %s/[bcdfghjklmnpqrstvwxz]\{3,}/\r====\0====\r/eig | execute 'g!/^====/d' | %s/=//eig
 
-for(var i of cons){
-for(var j of cons){
-for(var k of cons){
-	addpat('_'+i+'8'+j+'8'+k+'8');
-	addpat('8'+i+'8'+j+'8'+k+'_');
-}}}
+var i = 'bbr bfl bfr bll bmv bpl bpr bsc bscr bsq bst bstr btr cbnd ccb cch ccl ccr cdt cfl chd chf chl chm chn chr chs chth cst cth ctr cts dbl dbr dcl dcr ddl ddr dfl dfr dgl dgn dgr dmn dnd dnt dpl dpr dsc dscb dscr dsp dst dstr dtr dvm ffb ffl ffr fth gdn gdt ggl ggr ght gnb grdv grr gtr hchr hpl jdt kgs khl kth lcb lch lchr lcr lct lctr ldk ldr lfg lfl lfr lgs lhg llb llh lls lms lph lpr lps lpt lss lst ltb lth ltr lvb mbb mbd mbl mbr mbs mchr mcl mct mfl mfr mgl mgr mmm mnns mph mphr mpl mpn mpr mps mpt mptr msc mscr msp mst mstr mth mtr mtv nbl ncb nch nchr ncl nclm ncr nct nctm nctr nctv ndb ndd ndr ndz nfl nfr nft ngd ngh ngl ngnl ngr ngs ngth nkn nktr nnb nnd nphr npl npr nsb nsc nscb nsch nscr nsd nsf nsfr nsgr nsj nsk nsl nsm nsn nsp nspl nsq nsrh nss nssc nsscr nssp nst nstr nstrln nsv ntb ntc ntf nth nthr ntl ntm ntq ntr ntrb ntrp nts ntv pcrc pdst phc phl phlm phn phph phr phsl pht phth ppb pph ppl ppr ppst psc psq pst pstr ptb pth ptr rbb rbl rbs rcb rch rchm rcl rcr rcs rct rdb rddd rdp rdph rds rfl rfr rgb rgdnd rgl rgr rks rmb rmdn rmn rnm rns rnt rnw rph rpl rpr rps rpt rrb rrh rsb rsc rscr rsm rsp rsq rss rst rstr rtb rth rthm rthr rtn rtr rts rtt rvb rzh sbr scb sch schr scl scm scr sct sdr sgr spc sph sphr spl spn spqr spr srr ssb ssj ssm ssp sss sst sstr stb stf stg sth sthm sthr stj stk stl stm stn stp stq str strm stsc stscr stt tfr thb thf thl thm thn thph thr ths tpr trb tst ttb tth ttr vnc xcb xcl xcr xgl xpl xpr xsc xscr xsf xsp xspl xst xstr xtdll xth xtr'.split(' ');
+addpat.apply(null, i.map((x)=> '_' + x.replace(/(.)/g, '$18')));
+addpat.apply(null, i.map((x)=> x.replace(/(.)/g, '8$1')+'_'));
+
+// consonant special cases
 for(var i of 'bcdgpt'.split('')){
 for(var j of 'hlr'.split('')){
 	addpat('3'+i+'4'+j);
 }}
 
-addpat.apply(all, ('_su5pe6r5 _co6n5 _que6m5 5e6x5 _pe6r5'+a1+'mb _pe6r5amb'
+// special cases
+addpat.apply(null,('_su5pe6r5 _co6n5 _que6m5 5e6x5 _pe6r5'+a1+'mb _pe6r5amb'
 	+ ' _d'+e1+'5s6 _de5s6 _lon6g5'+a1+'6n _lon6g5a6n _a6b5u6nd').split(' '));
 
+// multi consonant overrides
 addpat('b5f6l', 'b5f6r', 'b5p6r', 'b5s6c', 'b6s5q', 'b5s6t', 'b5t6r',
 	'c5c6b', 'c5c6h', 'c5c6l', 'c5c6r', 'c5f6l', 'c6h5l', 'c6h5m', 'c6h5n',
 	'c5s6t', 'c5t6h', 'c5t6r', 'd5b6l', 'd5b6r', 'd5c6l', 'd5c6r', 'd5f6l',
@@ -104,11 +115,27 @@ var hyph = function(str, lang) {
 
 module.exports = hyph;
 
+var fs = require('fs');
+var path = require('path');
+var output = 'module.exports = ' + JSON.stringify(patLA, null, 3) + ';';
+var fname = path.join(__dirname, 'la.js');
+fs.writeFile(fname, output);
+
 /*
 var txt = 'amœ́na cǽsari judǽos judǽi judǽis judǽus sǽcula';
 txt = 'coagulátum exstíngue frigus fíguli fúlgura gubernáre insúrgunt jugum largus linguam linguis lingua pinguédine regum sanguínibus singuláriter sánguinem ánguli árguit';
 var htxt = hyph(txt)
 console.log(htxt);
 // */
-// %s/[ ():;.,?]\+/\r/g
+
+/* commands to get all words from brevrom and whitaker
+
+g/\v^(\[|!|\&|#|\$)/d
+g/=/d
+%s/{[^}]\+}\|_//eig | %s/\~//eig | %s/[;0-9]\+$//eig | %s/[0-9]\+//eig
+%s/[ ():;.,?"']\+/\r/g
+
+find . -iname '*.txt' > latin.vi
+find . -iname '*.txt' | sed -e 's/\(.*\)/iconv -t UTF-8 "\1"/e' > latin.vi
+*/
 
